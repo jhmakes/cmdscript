a08763ff49da8f2dca0d1284f01e96956df9740c
jhmakes <90417025+jhmakes@users.noreply.github.com>
2026-03-22 17:31:42 -0300

    Update Parser.js
    Add documentation.

--- src/Parser.js
+++ src/Parser.js
@@ -1,183 +1,79 @@
-var LexerIsLetter = function(c){
-    return c.toUpperCase() != c.toLowerCase();
-}
-var LexerIsDigit = function(c){
-    return '0'.charCodeAt(0) <= c.charCodeAt(0) && c.charCodeAt(0) <= '9'.charCodeAt(0);
-}
-var LexerIsSpace = function(c){
-    return c == " " || c == "\n" || c == "\t";
-}
-var LexerOpsit = {
-    "(": ")", ")": "(",
-    "<": ">", ">": "<",
-    "[": "]", "]": "[",
-    "{": "}", "}": "{"
-}
-var LexerOpposite = function(c){
-    return LexerOpsit[c];
-}
+var unit = [];
 
-var ParserExpect = function(src,c,msg){
-    if(src[0] != c[0])
-        throw msg;
-    src.shift();
-}
+/*
+                  Dispatch Definitions
 
-var ParseSpace = function(src){
-    while(src.length > 0 && LexerIsSpace(src[0]))
-        src.shift();
-}
+        The dispatch prioritise the entries with more given values
+    than the ones only with variables (fig.1):
 
-var ParseDigit = function(src){
-    var num = "";
-    while(src.length > 0 && LexerIsDigit(src[0]))
-        num += src.shift();
-    if(src.length > 0 && src[0] == '.'){
-        num += src.shift();
-        while(src.length > 0 && LexerIsDigit(src[0]))
-            num += src.shift();
-        return {
-            type: "float",
-            value: +num,
-            debugValue: num
-        }
-    }
+        #1 fac n = ...;
+               v
+            variable
+         
+        #2 fac 0 = ...; <- this will be read first.
+               v
+             value
+
+        The arity, number of parameters, of a function counts
+    to order the definitions too. Lower arities come before
+    higher arities (fig.2):
+
+        #1 f x y = ...; <- this go after #2.
+        #2 f x = ...; <- this go first.
+        #3 f x y 0 = ...; <- this go last.
+
+        The 'mdefs' variable holds an example of a function
+    definition. Each row has four columns, and they're
+    ordered following the table:
+
+        [arity,values (fig.1, #2), params, impl]
+
+        If any param in the 'params' column is 'null', then
+    it represents a variable (fig.3):
+
+        #1 f x 1 = ...;
+        #2  [null,1]
     
-    return {
-        type: "int",
-        value: (+num) | 0,
-        debugValue: num
+*/
+var mdefs = [[1,1,[unit],(a => 0)],
+             [1,0,[null],(a => a*a)],
+             [2,0,[null,null],((a,n) => a*n)],
+             [2,1,[null,0],((a,n) => a)],
+             [1,1,[0],(a => 1)]];
+
+/* Implementing first paragraph. */
+var toTable = function(defs){
+    var tabl = [];
+    // See fig.1 //
+    defs = defs.map(i => i).sort((a,b) => b[1] - a[1]);
+    // See fig.2 //
+    for(var df of defs){
+        if(tabl[df[0]] == undefined)
+            tabl[df[0]] = [];
+        tabl[df[0]].push(df);
     }
+    return tabl;
 }
 
-var ParseWord = function(src){
-    var wrd = src.shift();
-    var multi = false;
-    while(src.length > 0 && (LexerIsLetter(src[0]) || src[0] == "_" || src[0] == ".")){
-        if(src[0] == ".")
-            multi = true;
-        wrd += src.shift();
-    }
+/* Debug Helper */
+var logTable = tabl => tabl.forEach((row,idx) => { console.log(`Table Entry ${idx}:`)
+                                             row.forEach(col => console.log("| | ["+col[0]+", "+col[1]+"]")); });
+/* Dispatch Helper */
+var cmpDefs = (dfs,args) => dfs.reduce(((acc, par, idx) => ((par == null) ? true : par == args[idx]) && acc),true);
 
-    if(wrd[0] == ".")
-        return {
-            type: "member",
-            value: wrd.substring(1),
-            symbol: wrd
+/* Dispatch Mechanism */
+var dispatchTable = (tabl,args) => {
+    var row = tabl[args[0]];
+    if(row == undefined)
+        return [];
+    for(var df of row){
+        if(cmpDefs(df[2],args[1])){
+            return df[3].apply(null,args[1]);
         }
-    else if(wrd[0] == ":")
-        return {
-            type: "type var",
-            value: wrd.substring(1),
-            symbol: wrd
-        }
-    else if(wrd[0] != "." && wrd[0] != ":" && multi)
-        return {
-            type: "multi",
-            value: wrd.split('.'),
-            symbol: wrd
-        }
-    
-    if(wrd == "yes")
-        return {
-            type: "bool",
-            value: 1
-        }
-    else if(wrd == "no")
-        return {
-            type: "bool",
-            value: 0
-        }
-    else if(wrd == "nil")
-        return {
-            type: "nil",
-            value: null
-        }
-    return {
-        type: "word",
-        value: wrd
     }
+    return {what: () => "err"};
 }
 
-var ParseString = function(src,type){
-    var str = "";
-    src.shift();
-    while(src.length > 0 && src[0] != type){
-        if(src[0] == "\\" && src.length > 1){
-            src.shift();
-            if(src[0] == type){
-                str += type;
-                src.shift();
-            }
-            else if(src[0] == "n"){
-                str += "\n";
-                src.shift();
-            }
-            else if(src[0] == "t"){
-                str += "\t";
-                src.shift();
-            }
-            else
-                str += src.shift();
-            continue;
-        }
-        str += src.shift()
-    }
-    src.shift();
-    return {
-        type: "string",
-        value: str
-    }
-}
-
-var ParseList = function(src){
-    var body = [];
-    var type = src.shift();
-    while(src.length > 0 && src[0] != LexerOpposite(type)){
-        body.push(ParseExpr(src));
-        ParseSpace(src);
-    }
-    ParserExpect(src,LexerOpposite(type),"RT ERR: expct '"+LexerOpposite(type)+"'.");
-    return {
-        type: "list",
-        value: body,
-        symbol: type + LexerOpposite(type)
-    }
-}
-
-var ParseValue = function(src){
-    ParseSpace(src);
-    if(LexerIsDigit(src[0]))
-        return ParseDigit(src);
-    else if(LexerIsLetter(src[0]) || src[0] == "_" || src[0] == "." || src[0] == ":")
-        return ParseWord(src);
-    else if(src[0] == '"')
-        return ParseString(src,'"');
-    else if(src[0] == "'")
-        return ParseString(src,"'");
-    else if(LexerOpposite(LexerOpposite(src[0])) == src[0])
-        return ParseList(src)
-    else if(src.length == 0)
-        throw "RT ERR: expct val.";
-    else
-        throw "RT ERR: unknwn val type.";
-}
-
-var ParseExpr = function(src){
-    return ParseValue(src);
-}
-var ParseStats = function(src){
-    var body = [];
-    while(src.length > 0){
-        body.push(ParseExpr(src))
-        ParseSpace(src);
-    }
-    return {
-        type: "program",
-        body: body
-    }
-}
-
-var ParserRun = function(src){
-    return ParseStats(src.split(''));
-}
+/* Use Examples */
+logTable(toTable(mdefs));
+console.log(dispatchTable(toTable(mdefs),[2,[2,0]]));
